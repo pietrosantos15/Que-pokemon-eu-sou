d6bdfd3163581d48d6dc09978c52f449a4edbe61
pietrosantos15 <pietro.santos.senai@gmail.com>
2025-03-08 13:38:13 -0300

    read me lindo

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import requests, random
+
+app = Flask(__name__)
+
+API_ENDPOINT = "https://pokeapi.co/api/v2/pokemon/"
+
+traducao = {
+    "normal": "Normal",
+    "fighting": "Lutador",
+    "flying": "Voador",
+    "poison": "Venenoso",
+    "ground": "Terrestre",
+    "rock": "Pedra",
+    "bug": "Inseto",
+    "ghost": "Fantasma",
+    "steel": "Aço",
+    "fire": "Fogo",
+    "water": "Água",
+    "grass": "Grama",
+    "electric": "Elétrico",
+    "psychic": "Psíquico",
+    "ice": "Gelo",
+    "dragon": "Dragão",
+    "dark": "Sombrio",
+    "fairy": "Fada"
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+
+        idade = int(request.form['idade'])
+
+        if idade > 100 or idade < 1:
+            erro = "Idade inválida"
+            return render_template('index.html', erro=erro)
+        
+        numaleatorio = random.randint(1, 926)
+        pokemon = idade + numaleatorio
+        
+        response = requests.get(API_ENDPOINT + str(pokemon))
+
+        if response.status_code == 200:
+            data = response.json()
+            nome = data["name"].capitalize()
+            tipos = [traducao.get(t["type"]["name"], t["type"]["name"]) for t in data["types"]]  # aq ele pega os nome dos tipo
+            peso = data["weight"] / 10
+            altura = data["height"] * 10
+            habilidades = [h["ability"]["name"] for h in data["abilities"]] # precisa disso para pegar direto as habildiades
+            imagem = data["sprites"]["front_default"]
+
+            return render_template('index.html',nome=nome, idade=idade, tipos=tipos, peso=peso, altura=altura, habilidades=habilidades, imagem=imagem,)
+
+        
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,30 @@
+### Curso Técnico de Desenvolvimento de Sistemas - Senai Itapeva
+<h1 align="center"> Qual Pokémon Eu Sou? </h1>
+Este projeto é um sistema web construído utilizando o framework Flask e a API do PokéAPI. O objetivo é criar uma plataforma onde os usuários podem inserir sua idade e descobrir qual Pokémon corresponde a essa idade, com base em um cálculo aleatório.
+
+## Índice
+- [Funcionalidades](#funcionalidades)
+- [Tecnologias](#tecnologias)
+- [Autores](#autores)
+- [Status](#status)
+
+## Funcionalidades
+ - Inserir idade para descobrir um Pokémon
+ - Exibir informações do Pokémon, incluindo nome, tipo, peso, altura e habilidades
+ - Exibir imagem do Pokémon
+
+## Tecnologias
+
+![image](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+![image](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![image](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![image](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)
+![image](https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white)
+
+## Autores
+- Pietro Freire Rezende dos Santos - GitHub - [pietrosantos15](https://github.com/pietrosantos15)
+
+
+
+## Status
+✅ Concluído ✅
